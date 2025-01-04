539381832de92a556cb8e586a8e008648213e9de
el <somto.achu5@gmail.com>
2025-01-04 20:08:52 +0000

    added fix for model when importing files

--- nodes/custom_nodes.py
+++ nodes/custom_nodes.py
@@ -31,36 +31,36 @@ class Extract_mask_with_scrible_map:
             },
         }
 
-    RETURN_TYPES = ("IMAGE","IMAGE",)
+    RETURN_TYPES = ("IMAGE",)
     FUNCTION = "get_map"
     CATEGORY = "peakfiction/custom"
 
     def get_map(self, original_image, scribble_image):
         # Convert tensors to NumPy arrays
         # if not isinstance(original_image, np.ndarray):
-        original_image_ = np.clip(255.0 * original_image.cpu().numpy().squeeze(), 0, 255).astype(np.uint8)
+        original_image = np.clip(255.0 * original_image.cpu().numpy().squeeze(), 0, 255).astype(np.uint8)
         # if not isinstance(scribble_image, np.ndarray):
-        scribble_image_ = np.clip(255.0 * scribble_image.cpu().numpy().squeeze(), 0, 255).astype(np.uint8)
+        scribble_image = np.clip(255.0 * scribble_image.cpu().numpy().squeeze(), 0, 255).astype(np.uint8)
 
-        # # Debugging: Check the converted array shapes
-        # print("Original Image Shape:", original_image.shape)
-        # print("Scribble Image Shape:", scribble_image.shape)
+        # Debugging: Check the converted array shapes
+        print("Original Image Shape:", original_image.shape)
+        print("Scribble Image Shape:", scribble_image.shape)
 
-        # # Ensure images are in BGR format for OpenCV
-        # if original_image.shape[-1] == 4:  # RGBA
-        #     original_image = cv2.cvtColor(original_image, cv2.COLOR_RGBA2BGR)
-        # elif original_image.shape[-1] == 3:  # RGB
-        #     original_image = cv2.cvtColor(original_image, cv2.COLOR_RGB2BGR)
+        # Ensure images are in BGR format for OpenCV
+        if original_image.shape[-1] == 4:  # RGBA
+            original_image = cv2.cvtColor(original_image, cv2.COLOR_RGBA2BGR)
+        elif original_image.shape[-1] == 3:  # RGB
+            original_image = cv2.cvtColor(original_image, cv2.COLOR_RGB2BGR)
 
-        # if scribble_image.shape[-1] == 4:  # RGBA
-        #     scribble_image = cv2.cvtColor(scribble_image, cv2.COLOR_RGBA2BGR)
-        # elif scribble_image.shape[-1] == 3:  # RGB
-        #     scribble_image = cv2.cvtColor(scribble_image, cv2.COLOR_RGB2BGR)
+        if scribble_image.shape[-1] == 4:  # RGBA
+            scribble_image = cv2.cvtColor(scribble_image, cv2.COLOR_RGBA2BGR)
+        elif scribble_image.shape[-1] == 3:  # RGB
+            scribble_image = cv2.cvtColor(scribble_image, cv2.COLOR_RGB2BGR)
 
-        # Optionally use ExtractMaskFromScribbleMap
-        # mask = ExtractMaskFromScribbleMap.get_map(original_image, scribble_image)
+        Optionally use ExtractMaskFromScribbleMap
+        mask = ExtractMaskFromScribbleMap.get_map(original_image, scribble_image)
         # return (original_image, scribble_image, mask)
 
 
 
-        return (original_image, scribble_image)
+        return (mask)
