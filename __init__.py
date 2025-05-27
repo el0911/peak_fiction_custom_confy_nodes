7d5e8c3ad0bda30b678de772ff2acef3e5575090
el <somto.achu5@gmail.com>
2025-05-28 00:30:43 +0100

    added vggt load to the load

--- __init__.py
+++ __init__.py
@@ -1,9 +1,10 @@
 from .nodes.custom_nodes import Extract_mask_with_scrible_map
-from .nodes.custom_nodes import Cube_map_From_Panorama
+from .nodes.custom_nodes import Cube_map_From_Panorama, Load_model_from_memory
 
 NODE_CLASS_MAPPINGS = {
     "Extract_mask_with_scrible_map": Extract_mask_with_scrible_map,
-    "PanoramaToCubemap": Cube_map_From_Panorama
+    "PanoramaToCubemap": Cube_map_From_Panorama,
+    "Load_model_from_memory":Load_model_from_memory
     # "ImageBlender": ImageBlender
 }
 
@@ -11,6 +12,7 @@ NODE_CLASS_MAPPINGS = {
 NODE_DISPLAY_NAME_MAPPINGS = {
     "Extract_mask_with_scrible_map": "Extract mask with scrible map",
     "PanoramaToCubemap": "Panorama to Cubemap Converter",
+    "Load_model_from_memory":"Load model vggt model from memory"
     # "ImageBlender": "Image Blender"
 }
 
